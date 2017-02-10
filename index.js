0dfdd7069c1fda71e48c325e3060fd5bb283e7af
rommoise <rommoise@lab126.com>
2017-02-10 09:59:55 -0800

    Small documentation

--- index.js
+++ index.js
@@ -18,5 +18,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+/** @type {Cache} */
 const Cache = require('./src/Cache');
 module.exports = Cache;
